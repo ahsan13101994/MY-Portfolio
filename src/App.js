118308c9dc79d29e803f62ce55831c96d8383492
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-22 17:35:05 -0400

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Header />
+        <Navbar />
+        <About />
+        <Projects />
+        <Resume />
+        <Contact />
+        <Portfolio />
+        <Footer />
+    </main>
+    </Router>
   );
 }
 
