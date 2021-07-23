67e87134ab3bd1dccfd6320109308915adfa549f
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-23 18:31:57 -0400

    fixed css

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ export default function About() {
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="../public/profile.jpg"
+                src="./profile.jpg"
               />
             </div>
           </div>
