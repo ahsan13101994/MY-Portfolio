55ecd988fdca55931d21cb27631fcd780dc85aca
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-22 23:13:05 -0400

    fixed bug, Added resume and css

--- src/data.js
+++ src/data.js
@@ -6,8 +6,7 @@ export const portfolio = [
         "This budget tracker app allows the user to add and subtract funds from their tracker with or without a connection. Entering transactions offline, user's data will populate the total when brought back online.This application uses IndexDB, Service Workers and Web Manifest to enable offline functionality.",
       image: "./project-1.gif",
       link: "https://budget1994.herokuapp.com/",
-      deploy:"",
-      Repo:"",
+      repo:"https://github.com/ahsan13101994/Progressive_Budget",
     },
     {
       title: "Plan Your Trip",
@@ -16,7 +15,7 @@ export const portfolio = [
         "As a team, we wanted to build an app, where you can plan for, and keep track of travel expenses, and have the ability to convert into different currencies. We made an app that gives you the ability to create a profile, where you can add trip budget cards, destinations and traveller advice on those destinations for other users to see.",
       image: "./landing-page.png",
       link: "https://intense-savannah-61016.herokuapp.com/",    
-      Repo:"https://github.com/darylnauman/cold-travel-planner",
+      repo:"https://github.com/darylnauman/cold-travel-planner",
     },
     {
       title: "E-commerce Backend Insomnia",
@@ -25,29 +24,33 @@ export const portfolio = [
         "This ORM application that allows users to API POST, PUT, and DELETE routes in Insomnia Core.",
       image: "./project-3.gif",
       link: "https://github.com/ahsan13101994/E-commerce_back-End",
+      repo: "https://github.com/ahsan13101994/E-commerce_back-End",
     },
     {
       title: "Code Refactor Assignment",
       subtitle: "HTML, CSS",
       description:
         "Simple HTML and CSS cleanup, for an existing client.",
-      image: "./project-4.gif",
+      image: "./project_3.JPG",
       link: "https://ahsan13101994.github.io/code_refactor_assignment/",
+      repo: "https://github.com/ahsan13101994/code_refactor_assignment",
     },
     {
-        title: "Code Refactor Assignment",
+        title: "Weather Dashboard",
         subtitle: "Java, API",
         description:
           "https://ahsan13101994.github.io/weather_dashboard/",
-        image: "./project-4.gif",
+        image: "./project_1.JPG",
         link: "https://ahsan13101994.github.io/weather_dashboard/",
+        repo: "https://github.com/ahsan13101994/weather_dashboard",
       },
       {
         title: "Work Day Planner",
         subtitle: "HTML, CSS",
         description:
           "Simple HTML and CSS cleanup, for an existing client.",
-        image: "./project-4.gif",
+        image: "./project_4.JPG",
         link: "https://ahsan13101994.github.io/work_dayplanner/",
+        repo: "https://github.com/ahsan13101994/work_dayplanner",
       },
   ];
