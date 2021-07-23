05c8322d128a21bccf3c24b216b9582faf03fe83
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-23 00:12:18 -0400

    css issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../index.css";
 export default function Footer() {
     return (
         <footer className="w-100 mt-auto text-dark p-4">
-            <div className="social">
+            <div className="container text-center mb-5">
                     <a href="https://github.com/ahsan13101994" class="fa fa-github"></a>
                     <a href="https://ca.linkedin.com/in/syed-ahsan-rizvi-730778181" class="fa fa-linkedin"></a>
             </div>  
