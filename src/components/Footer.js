0d195ca0a2b1e175d64ddf111d104c8115c7f39f
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-22 23:15:56 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import "../index.css";
 
-export default function Footer() {}
+export default function Footer() {
+    return (
+        <footer className="w-100 mt-auto text-dark p-4">
+            <div className="social">
+                    <a href="https://github.com/ahsan13101994" class="fa fa-github"></a>
+                    <a href="https://ca.linkedin.com/in/syed-ahsan-rizvi-730778181" class="fa fa-linkedin"></a>
+            </div>  
+          <div className="container text-center mb-5">
+            <h4>&copy; 2021 | Syed Ahsan Rizvi. All Rights reserved</h4>
+          </div>
+        </footer>
+      );
+}
