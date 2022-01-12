045b8f560e1997dbc764f7edeb3da31b0b4d9d0a
Syed Ahsan Rizvi <83813204+ahsan13101994@users.noreply.github.com>
2022-01-11 20:23:36 -0500

    Title changes in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,13 @@ export default function About() {
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                 Hi, I'm Syed Ahsan Rizvi.
-                <br className="hidden lg:inline-block" /> Working to become a 
+                <br className="hidden lg:inline-block" /> 
                 Full Stack Developer
               </h1>
               <p className="mb-8 leading-relaxed">
               Based in Toronto Canada. 
-             The Full Stack Developer from University of Toronto. 
-                I am an ex-firefighter in Emergency Services Rescue 1122, now making a career switch due to my increasing interest in coding and web development.
+              I am a Full Stack Developer, graduated from University of Toronto Bootcamp. 
+              I am an ex-firefighter in Emergency Services Rescue 1122, now making a career switch due to my increasing interest in coding and web development.
               </p>
               <div className="flex justify-center">
                 <a
